5da2955f84c5e5f6591d535b74e6a594c42381a7
Samantha John and Trevor John <samantha.m.john@gmail.com>
2011-12-25 18:11:01 -0500

    Fake athlete data gem.

---
+++ lib/lorem_athlete.rb
@@ -0,0 +1,80 @@
+require 'YAML'
+lib_path = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib_path)
+class LoremAthlete
+  WORDS = YAML::load(File.open('lib/words.yml'))
+  def self.sport
+    sports = YAML::load(File.open('lib/sports.yml'))
+    sports[rand(sports.length)]
+  end
+
+  def self.athlete
+    athletes = YAML::load( File.open('lib/athletes.yml'))
+    athletes[rand(athletes.length)]
+  end
+
+  def self.team(sport='all')
+    pro_sports_teams = YAML::load( File.open('lib/teams.yml'))
+    case sport.downcase
+    when "all"
+      random_index = rand(2)
+      if random_index == 0
+        teams = pro_sports_teams.map{|k, v| v}.flatten
+      else
+        teams = olympic_sport(self.sport)
+      end
+    when "football"
+      teams = pro_sports_teams["football"]
+    when "basketball"
+      teams = pro_sports_teams["basketball"]
+    when "hockey"
+      teams = pro_sports_teams["hockey"]
+    when "baseball"
+      teams = pro_sports_teams["baseball"]
+    else
+      teams = olympic_sport(sport)
+    end
+    teams[rand(teams.length)]
+  end
+
+  def self.word
+    (WORDS["nouns"] + WORDS["verbs"])[rand(WORDS["nouns"].length + WORDS["verbs"].length)]
+  end
+
+  def self.sentence
+    num_words = rand(2) + 4
+    sentence = num_words.times.map{|word| "#{sentence_word}"}
+    sentence.join(" ") + "."
+  end
+
+  def self.paragraph(length=4)
+    length.times.map{|word| "#{sentence}"}.join(' ')
+  end
+
+  private
+  def self.country
+    countries  = YAML::load(File.open("lib/countries.yml"))
+    countries[rand(countries.length)]
+  end
+
+  def self.olympic_sport(olympic_sport)
+    ["#{self.country} #{["Mens", "Womens", "Mixed"][rand(3)]} Olympic #{olympic_sport} Team"]
+  end
+
+  def self.sentence_word
+    word_type = ["noun", "verb", "adverb", "athlete", "team", "noun", "verb", "athlete", "team"][rand(9)]
+    case word_type
+    when "noun"
+      WORDS["nouns"][rand(WORDS["nouns"].length)]
+    when "verb"
+      WORDS["verbs"][rand(WORDS["verbs"].length)]
+    when "adverb"
+      WORDS["adverbs"][rand(WORDS["adverbs"].length)]
+    when "athlete"
+      self.athlete
+    when "team"
+      self.team
+    end
+  end
+
+end
---
+++ lorem_athlete.gemspec
@@ -0,0 +1,10 @@
+Gem::Specification.new do |s|
+  s.name = 'lorem_athlete'
+  s.version = '0.0.1'
+  s.date = '2011-12-25'
+  s.authors = ["Samantha John", "Trevor John"]
+  s.email = 'trevorrjohn@gmail.com'
+  s.summary = 'A gem for generating fake sports data.'
+  s.description = "Use this in your tests to come up with fake athletes, teams, sports and commentary."
+  s.files = ["README.md", "lib/lorem_athlete.rb", "lib/teams.yml", "lib/countries.yml", "lib/words.yml", "lib/sports.yml", "lib/athletes.yml"]
+end
